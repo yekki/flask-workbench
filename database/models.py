5c29cc2e7f4efa52ddb67c6d1649bf0b254db8dd
Gary Niu <gary.niu@gmail.com>
2018-09-30 14:10:56 +0800

    update

--- database/models.py
+++ database/models.py
@@ -3,9 +3,28 @@
 
 from common.exts import db
 
+__all__ = ['User']
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
+    profile = db.relationship('Profile', backref='user', uselist=False, cascade='delete')
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    address = db.Column(db.String(255))
+    phone = db.Column(db.String(20))
+    uid = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+def load_data():
+    db.session.query(User).delete()
+    for i in range(5):
+        user = User(username=f'user{i}', password='password')
+        user.profile = Profile(address='Shanghai', phone='13816807866', email=f'user{i}@me.com')
+        db.session.add(user)
+    db.session.commit()
--- database/views.py
+++ database/views.py
@@ -3,20 +3,15 @@
 
 from flask import render_template, url_for, redirect
 from flask.views import MethodView
-from flask_table import LinkCol
 
 from common.exts import db
 from database import db_bp
-from database.models import User
-from database.widgets import UserTable
+from database.models import User, load_data
 
 
 class HomeView(MethodView):
     def get(self):
-        users = User.query.all()
-        table = UserTable(items=users)
-        table.add_column('action_panel', LinkCol('删除', endpoint='db.user', url_kwargs=dict(id='id')))
-        return render_template('database/index.html', table=UserTable(items=users))
+        return render_template('database/index.html', users=User.query.all())
 
 
 class UserView(MethodView):
@@ -27,14 +22,10 @@ class UserView(MethodView):
 
 
 def load():
-    for i in range(5):
-        db.session.add(User(username=f'user{i}', password='password', email=f'user{i}@me.com'))
-
-    db.session.commit()
+    load_data()
     return redirect(url_for('db.home'))
 
 
 db_bp.add_url_rule('/', view_func=HomeView.as_view('home'))
 db_bp.add_url_rule('/load', view_func=load, endpoint='load')
 db_bp.add_url_rule('/user/<int:id>', view_func=UserView.as_view('user'))
-
